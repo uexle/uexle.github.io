b48658b1c7dd5e7e5aa974ca401762adb916fc00
uexle <wesleivianna235@gmai.com>
2024-03-05 15:24:24 -0300

    spelling correction

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-//  botao de trocar iluminacao do site
+//botao de trocar iluminacao do site
 function trocaModo() {
-    let html = document.documentElement
-    html.classList.toggle("dark")
+  let html = document.documentElement;
+  html.classList.toggle("dark");
 }
 
-//  para o carrossel funcionar e não quebrar, é preciso que a div
-//  dos logos esteja duplicada e só então adicionar a animação
+//para o carrossel funcionar e não quebrar, é preciso que a div
+//dos logos esteja duplicada e só então adicionar a animação
 function auxiliarCarrossel() {
-    const carrossel = document.querySelector(".carrossel")
-    let copia = document.querySelector(".carrossel-logos").cloneNode(true)
-    carrossel.appendChild(copia)
+  const carrossel = document.querySelector(".carrossel");
+  let copia = document.querySelector(".carrossel-logos").cloneNode(true);
+  carrossel.appendChild(copia);
 
-    const logoDivs = document.querySelectorAll(".carrossel-logos")
-    logoDivs.forEach(div => {
-        div.style.animation = '20s linear 0s infinite normal none running slide'
-    })
+  const logoDivs = document.querySelectorAll(".carrossel-logos");
+  logoDivs.forEach((div) => {
+    div.style.animation = "20s linear 0s infinite normal none running slide";
+  });
 
-    carrossel.addEventListener("mouseover", function () {
-        logoDivs.forEach(div => {
-            div.style.animation = '20s linear 0s infinite normal none paused slide'
-        })
-    })
+  carrossel.addEventListener("mouseover", function () {
+    logoDivs.forEach((div) => {
+      div.style.animation = "20s linear 0s infinite normal none paused slide";
+    });
+  });
 
-    carrossel.addEventListener("mouseleave", function () {
-        logoDivs.forEach(div => {
-            div.style.animation = '20s linear 0s infinite normal none running slide'
-        })
-    })
+  carrossel.addEventListener("mouseleave", function () {
+    logoDivs.forEach((div) => {
+      div.style.animation = "20s linear 0s infinite normal none running slide";
+    });
+  });
 }
 
-//  evitar que as imagens possam ser arrastadas
+//evitar que as imagens possam ser arrastadas
 function evitaDrag() {
-    const elementos = document.querySelectorAll("img")
-    elementos.forEach(elemento => {
-        elemento.setAttribute("draggable", "false")
-    })
+  const elementos = document.querySelectorAll("img");
+  elementos.forEach((elemento) => {
+    elemento.setAttribute("draggable", "false");
+  });
 }
-
 
 document.addEventListener("DOMContentLoaded", function () {
-    auxiliarCarrossel()
-    evitaDrag()
-})
+  auxiliarCarrossel();
+  evitaDrag();
+});
